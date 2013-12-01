944b1e668462b5321127fe3e8a189fa31d2928a1
Hyon S Chu <eatkimchi@outlook.com>
2013-12-01 01:56:26 -0800

    turned into function, includes "100" as a guess

--- number guessing game.py
+++ number guessing game.py
@@ -1,27 +1,28 @@
 from random import randrange
-totalrange=100
-print "Please think of a number between 0 and %s" %totalrange
-rng = randrange(totalrange+1)
-guess = False
-minrange=0
-maxrange=totalrange
-guesses = 1
-print "is your secret number %s ?" %rng
-while guess == False:
-        ans = raw_input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
-        if ans == "l":
-                minrange = rng+1
-                rng = randrange(minrange, maxrange)
-                print "is your number %s" %rng
-                guesses += 1
-        elif ans == "h":
-                maxrange = rng
-                rng = randrange(minrange,maxrange)
-                print "is your number %s" %rng
-                guesses += 1
-        elif ans == "c":
-                guess=True
-                break
-        else:
-                print "Sorry, I did not understand your input."
-print "\nGame over. Your secret number was: %s \nIt took %s number of guesses" % (rng, guesses)
+def guessme():
+        totalrange=101
+        print "Please think of a number between 0 and %s" %totalrange
+        rng = randrange(totalrange+1)
+        guess = False
+        minrange=0
+        maxrange=totalrange
+        guesses = 1
+        print "is your secret number %s ?" %rng
+        while guess == False:
+                ans = raw_input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+                if ans == "l":
+                        minrange = rng+1
+                        rng = randrange(minrange, maxrange)
+                        print "is your number %s" %rng
+                        guesses += 1
+                elif ans == "h":
+                        maxrange = rng
+                        rng = randrange(minrange,maxrange)
+                        print "is your number %s" %rng
+                        guesses += 1
+                elif ans == "c":
+                        guess=True
+                        break
+                else:
+                        print "Sorry, I did not understand your input."
+        print "\nGame over. Your secret number was: %s \nIt took %s number of guesses" % (rng, guesses)
