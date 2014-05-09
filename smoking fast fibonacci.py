ac8cdce3822bea76ec5810dee67e48d155d5df5d
Hyon S Chu <eatkimchi@outlook.com>
2014-05-09 01:23:16 -0700

    Update smoking fast fibonacci.py

--- smoking fast fibonacci.py
+++ smoking fast fibonacci.py
@@ -2,11 +2,11 @@
 # 100 loops, best of 3: 3.79 ms per loop
 
 def fib(length):
-  first, second = 1, 1
-  if length < 2:
-    return length
-  else:
-    length -= 2
-    for i in range(length):
-      second, first = first+second, second
-  return second
+	first, second = 1, 1
+	if length < 2:
+		return length
+	else:
+		length -= 2
+		for i in range(length):
+			second, first = first+second, second
+		return second
