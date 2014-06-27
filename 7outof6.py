0bc381e96d6ec15027f05cf66bf0a4bb44594fdc
Hyon S Chu <eatkimchi@outlook.com>
2014-06-27 01:36:13 -0700

    created 7outof6.py
    Generating a number from 1:7 using a 6-sided die.

---
+++ 7outof6.py
@@ -0,0 +1,17 @@
+import random 
+# import this to use the random feature
+my_set = [ [] for i in range(7) ] 
+# make 7 empty lists
+no_of_rolls = 100 
+# the larger the better, but no need to get too large (> 500)
+list_id = 0
+# this is used to identify to a list in my_set
+for i in range(no_of_rolls)*7:
+  roll_outcome = int(random.random()*7)
+  my_set[list_id].append(roll_outcome)
+  list_id += 1
+  if list_id > 6:
+    list_id = 0
+winner = [sum(i) for i in my_set ]
+print winner
+print max(winner), ", random number:", winner.index(max(winner))+1
